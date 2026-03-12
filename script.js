5507410b54578a2a2742c6ead3a307c15dd8cc2f
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-03-12 14:17:02 +0400

    Fixed grid layout and make the page responsive

---
+++ script.js
@@ -0,0 +1,25 @@
+// Get the Elments from the document
+
+
+
+// Fetch data from json file
+
+fetch("./data.json").then((response) => { 
+    if(!response)
+        return console.log("There is no data");
+
+    return response.json();
+}).then((data) => { 
+   
+})
+
+
+function DisplayDaily() { 
+
+}
+
+
+
+
+
+
