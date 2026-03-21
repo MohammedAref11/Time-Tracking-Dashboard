b4903c8066c012e47b41adfe332dcd858a1c41e3
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-03-21 18:59:47 +0400

    Added error handling

--- script.js
+++ script.js
@@ -1,70 +1,54 @@
-// buttons Elements
+//* buttons Elements
 const dailyBtn = document.getElementById("dailyBtn");
 const weeklyBtn =  document.getElementById("weeklyBtn");
 const monthlyBtn = document.getElementById("monthlyBtn");
-
-// Cards elements
+//* Cards elements
 const current = document.querySelectorAll(".dashboard__card--hours");
 const prev = document.querySelectorAll(".dashboard__card--previous");
 
 fetch("./data.json").then((response) => { 
     if(!response.ok) { 
-        return console.log("There is no data")
+        throw new Error("Couldn't Get the files")
     }
 
     return response.json()
 
 }).then((el) => { 
 
-    // Display weekly data as default 
+    //* Display weekly data as default 
     current.forEach((item, index) => { 
             item.textContent = `${el[index].timeframes.weekly.current}hrs`;
-        })
+        });
     prev.forEach((item, index) => { 
             item.textContent = `Last week - ${el[index].timeframes.weekly.previous}hrs`;
-        })
+        });
     // Display daily data when daily button is clicked
    dailyBtn.addEventListener('click', () => { 
         current.forEach((item, index) => { 
             item.textContent = `${el[index].timeframes.daily.current}hrs`;
-        })
+        });
         prev.forEach((item, index) => { 
             item.textContent = `Yesterday - ${el[index].timeframes.daily.previous}hrs`;
-        })
+        });
    })
-    // Display weekly data when weekly button is clicked
+    //* Display weekly data when weekly button is clicked
    weeklyBtn.addEventListener('click', () => { 
         current.forEach((item, index) => { 
             item.textContent = `${el[index].timeframes.weekly.current}hrs`;
-        })
+        });
         prev.forEach((item, index) => { 
             item.textContent = `Last week - ${el[index].timeframes.weekly.previous}hrs`;
-        })
+        });
    })
-    // Display monthly data when monthly button is clicked
+    //* Display monthly data when monthly button is clicked
    monthlyBtn.addEventListener('click', () => { 
         current.forEach((item, index) => { 
             item.textContent = `${el[index].timeframes.monthly.current}hrs`;
-        })
+        });
         prev.forEach((item, index) => { 
             item.textContent = `Last month - ${el[index].timeframes.monthly.previous}hrs`;
-        })
+        });
    })
+}).catch((error) => { 
+    console.log(error)
 })
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
